9e9223869674ffd699c20a086012abd941a7b589
Mike Malyshev <mmisha28@gmail.com>
2025-02-03 16:42:11 +0000

    fixed backend

--- src/app/api/enhance/route.ts
+++ src/app/api/enhance/route.ts
@@ -2,16 +2,18 @@ import { NextRequest, NextResponse } from 'next/server';
 import openai from '@/utils/openaiClient';
 import { SYSTEM_PROMPT } from './systemPrompt';
 export async function POST(req: NextRequest) {
-	return NextResponse.json({
-		hasError: true,
-		text: 'Also have experience working in a small startup.',
-		error:
-			"1) 'at working' should be just 'working' because 'have experience working' is the correct form.<br>" +
-			"2) 'in small startup' should be 'in a small startup' because 'startup' is a countable noun and requires an article 'a'.",
-		enchancedText: 'I also have experience working in a small startup.',
-		formal: 'I also possess experience working within a small startup.',
-		informal: "I've worked in a small startup, too.",
-	});
+	if (process.env.NODE_ENV === 'development') {
+		return NextResponse.json({
+			hasError: true,
+			text: 'Also have experience working in a small startup.',
+			error:
+				"1) 'at working' should be just 'working' because 'have experience working' is the correct form.<br>" +
+				"2) 'in small startup' should be 'in a small startup' because 'startup' is a countable noun and requires an article 'a'.",
+			enchancedText: 'I also have experience working in a small startup.',
+			formal: 'I also possess experience working within a small startup.',
+			informal: "I've worked in a small startup, too.",
+		});
+	}
 
 	try {
 		const { text } = await req.json();
